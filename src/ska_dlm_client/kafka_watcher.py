04b6a075c1276071c9b053a31ee1bbc4b3f22ea9
Nadia Steyn <nadia.steyn.bsc@gmail.com>
2024-09-26 16:50:04 +0800

    YAN-1859: Starting a script for the Kafka consumer

---
+++ src/ska_dlm_client/kafka_watcher.py
@@ -0,0 +1,73 @@
+"""Kafka watcher client."""
+
+import argparse
+import asyncio
+import json
+import logging
+import time
+
+import aiokafka
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    """Control the main execution of the program."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--kafka-server",
+        nargs="*",
+        default=["localhost:9092"],
+        help="The host:port of the Kafka server(s) to bootstrap from",
+    )
+    parser.add_argument(
+        "--kafka-topic",
+        nargs="*",
+        default=["ska-sdp-dataproduct-ingest"],
+        help="The Kafka topic(s) to watch",
+    )
+
+    args = parser.parse_args()
+
+    asyncio.run(_start_watcher(args))
+
+
+async def _try_start_consumer(consumer: aiokafka.AIOKafkaConsumer):
+    """Start the Kafka consumer."""
+    try:
+        await consumer.start()
+        return True
+    except aiokafka.errors.KafkaError:
+        return False
+
+
+async def _start_watcher(args):
+    """Start watching the given Kafka topic."""
+    logger.debug("Connecting to Kafka server(s): %s", ", ".join(args.kafka_server))
+    logger.info("Watching %s topic(s) for dataproducts to process", ", ".join(args.kafka_topic))
+
+    consumer = aiokafka.AIOKafkaConsumer(*args.kafka_topic, bootstrap_servers=args.kafka_server)
+
+    try:
+        attempts = 0
+        while not await _try_start_consumer(consumer):
+            attempts += 1
+            if attempts > 5:
+                raise RuntimeError(f"Unable to connect to {args.kafka_server}")
+            time.sleep(1)
+
+        async for msg in consumer:
+            try:
+                data = json.loads(msg.value)
+                logger.info("Consuming message: %s", data)
+            except json.JSONDecodeError:
+                logger.error("Unable to parse JSON")
+                continue
+
+    finally:
+        await consumer.stop()
+
+
+if __name__ == "__main__":
+    # NOTE: we call main() here, and then let main() call _start_watcher()
+    main()
