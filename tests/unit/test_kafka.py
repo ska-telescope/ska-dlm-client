76743406b66b2850e75b8b2e73a91cf1eb10a82d
Nadia Steyn <nadia.steyn.bsc@gmail.com>
2024-10-07 14:44:46 +0800

    YAN-1859: Simple unit test for Kafka, borrowed from ska-sdp-lmc-queue-connector

---
+++ tests/unit/test_kafka.py
@@ -0,0 +1,58 @@
+import asyncio
+
+import pytest
+import pytest_asyncio
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+
+KAFKA_HOST = "localhost:9092"
+TEST_TOPIC = "test-events"
+
+
+@pytest_asyncio.fixture(name="producer")
+async def producer_fixture():
+    p = AIOKafkaProducer(bootstrap_servers=KAFKA_HOST)
+    await p.start()
+    yield p
+    await p.stop()
+
+
+@pytest_asyncio.fixture(name="consumer")
+async def consumer_fixture():
+    consumer = AIOKafkaConsumer(TEST_TOPIC, bootstrap_servers=KAFKA_HOST)
+    await consumer.start()
+    yield consumer
+    await consumer.stop()
+
+
+@pytest.mark.asyncio
+async def test_kafka_data_roundtrip(producer: AIOKafkaProducer, consumer: AIOKafkaConsumer):
+    messages = ["one", "two", "three"]
+
+    tasks = [
+        asyncio.create_task(send_messages(producer, messages)),
+        asyncio.create_task(receive_messages(consumer, max_num_messages=3)),
+    ]
+    done, pending = await asyncio.wait(tasks, timeout=5)
+
+    assert len(pending) == 0
+    assert len(done) == 2
+    for task in done:
+        assert task.exception() is None
+
+    received_messages: list[bytes] = tasks[1].result()
+    assert len(received_messages) == 3
+    for expected_str, received_bytes in zip(messages, received_messages):
+        assert expected_str.encode() == received_bytes
+
+
+async def send_messages(producer: AIOKafkaProducer, msgs: list[str]):
+    for msg in msgs:
+        await producer.send_and_wait(TEST_TOPIC, msg.encode())
+
+
+async def receive_messages(consumer: AIOKafkaConsumer, max_num_messages: int) -> list[str]:
+    messages = []
+    async for msg in consumer:
+        messages.append(msg.value)
+        if len(messages) == max_num_messages:
+            return messages
