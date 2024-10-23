77902c205e200b91fa3f2c6034618f26ef01309c
Mark Boulton <mark.boulton@uwa.edu.au>
2024-10-23 18:12:55 +0800

    YAN-1823: WIP and refactor.

--- src/ska_dlm_client/directory_watcher/directory_monitor.py
+++ src/ska_dlm_client/directory_watcher/directory_monitor.py
@@ -3,7 +3,6 @@
 import asyncio
 import json
 import logging
-import sys
 import time
 from enum import Enum
 from pathlib import Path
@@ -18,9 +17,14 @@ from ska_dlm_client.directory_watcher.directory_watcher_entry import (
     DirectoryWatcherEntries,
     DirectoryWatcherEntry,
 )
+from ska_dlm_client.directory_watcher.testing import (
+    init_location_for_testing,
+    init_storage_for_testing,
+)
 from ska_dlm_client.openapi import api_client, configuration
-from ska_dlm_client.openapi.dlm_api import ingest_api, storage_api
+from ska_dlm_client.openapi.dlm_api import ingest_api
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 ingest_configuration = configuration.Configuration(host=DLMConfiguration.INGEST_URL)
 storage_configuration = configuration.Configuration(host=DLMConfiguration.STORAGE_URL)
@@ -54,7 +58,7 @@ def follow_sym_link(path: Path) -> Path:
         path.is_file()
 
 
-def register_entry(entry_path: Path):
+def register_entry(entry_path: Path, relative_path: str):
     """Register the given entry_path."""
     full_path = entry_path.resolve()
     # is_measurement_set = False
@@ -65,14 +69,11 @@ def register_entry(entry_path: Path):
     with api_client.ApiClient(ingest_configuration) as ingest_api_client:
         api_ingest = ingest_api.IngestApi(ingest_api_client)
         response = api_ingest.register_data_item_ingest_register_data_item_post(
-            item_name="",
-            uri=full_path,
-            storage_name=None,
-            storage_id="needed",
-            item_format=None,
-            eb_id="needed????",
-            authorization="bearer token",
-            body="what is this meant to be ???",
+            item_name=relative_path,
+            uri=relative_path,
+            storage_name=WatchConfiguration.STORAGE_NAME,
+            storage_id=storage_id,
+            eb_id=WatchConfiguration.EB_ID,
         )
     # TODO: decode JSON response
     json.dumps(response)
@@ -91,12 +92,13 @@ def register_entry(entry_path: Path):
 def do_something_with_new_entry(entry: tuple[Change, str]):
     """TODO: Test function currently."""
     logger.info("in do something %s", entry)
-    entry_path = Path(entry[1])
+    relative_path = entry[1].replace(f"{WATCH_DIRECTORY}/", "")
+    entry_path = Path(relative_path)
     logger.info(entry_path)
     #    pp = PosixPath(entry[1])
     #    print(pp)
     if entry[0] is Change.added:
-        register_entry(entry_path)
+        register_entry(entry_path, relative_path)
     if entry[0] is not Change.deleted:
         path = entry_path.resolve()
         if path.exists():
@@ -104,82 +106,6 @@ def do_something_with_new_entry(entry: tuple[Change, str]):
             logger.info(stat)
         else:
             logger.info("Cannot find resolved path %s", path)
-
-
-def init_location_for_testing() -> str:
-    """Perform location initialisation to be used when testing."""
-    with api_client.ApiClient(storage_configuration) as the_api_client:
-        api_storage = storage_api.StorageApi(the_api_client)
-
-        # get the location_id
-        response = api_storage.query_location_storage_query_location_get(
-            location_name=WatchConfiguration.LOCATION_NAME
-        )
-        logger.info("query_location_response: %s", response)
-        if not isinstance(response, list):
-            logger.error("Unexpected response from query_location_storage")
-            sys.exit(1)
-        if len(response) == 1:
-            the_location_id = response[0]["location_id"]
-        else:
-            response = api_storage.init_location_storage_init_location_post(
-                location_name=WatchConfiguration.LOCATION_NAME,
-                location_type=WatchConfiguration.LOCATION_TYPE,
-                location_country=WatchConfiguration.LOCATION_COUNTRY,
-                location_city=WatchConfiguration.LOCATION_CITY,
-                location_facility=WatchConfiguration.LOCATION_FACILITY,
-            )
-            the_location_id = response
-        logger.info("location_id: %s", the_location_id)
-    return the_location_id
-
-
-def init_storage_for_testing(the_location_id: str) -> str:
-    """Perform storge initialisation to be used when testing."""
-    assert the_location_id is not None
-    with api_client.ApiClient(storage_configuration) as the_api_client:
-        api_storage = storage_api.StorageApi(the_api_client)
-        # Get the storage_id
-        response = api_storage.query_storage_storage_query_storage_get(
-            storage_name=WatchConfiguration.STORAGE_NAME
-        )
-        if not isinstance(response, list):
-            logger.error("Unexpected response from query_storage_storage")
-            sys.exit(1)
-        if len(response) == 1:
-            the_storage_id = response[0]["storage_id"]
-        else:
-            response = api_storage.init_storage_storage_init_storage_post(
-                storage_name=WatchConfiguration.STORAGE_NAME,
-                location_name=WatchConfiguration.LOCATION_NAME,
-                location_id=the_location_id,
-                storage_type=WatchConfiguration.STORAGE_TYPE,
-            )
-            the_storage_id = response
-        logger.info("storage_id: %s", the_storage_id)
-
-        # Setup the storage config
-        response = api_storage.create_storage_config_storage_create_storage_config_post(
-            storage_id=the_storage_id, config=WatchConfiguration.STORAGE_CONFIG
-        )
-        storage_config_id = response
-        logger.info("storage_config_id: %s", storage_config_id)
-    return the_storage_id
-
-
-def test_ingest_item():
-    """Test ingesting a single item."""
-    with api_client.ApiClient(ingest_configuration) as the_api_client:
-        api_ingest = ingest_api.IngestApi(the_api_client)
-        the_path = "k"
-        response = api_ingest.register_data_item_ingest_register_data_item_post(
-            item_name=the_path,
-            uri=the_path,
-            storage_name=WatchConfiguration.STORAGE_NAME,
-            storage_id=storage_id,
-            eb_id=WatchConfiguration.EB_ID,
-        )
-        logger.info("register_data_item_response: %s", response)
 
 
 async def main():
@@ -194,8 +120,8 @@ WATCH_DIRECTORY = WatchConfiguration.DIRECTORY_TO_WATCH
 
 # TODO: It would be expected that the following config would already be
 # completed in prod but leaving in place for now.
-location_id = init_location_for_testing()
-storage_id = init_storage_for_testing(the_location_id=location_id)
-test_ingest_item()
+location_id = init_location_for_testing(storage_configuration)
+storage_id = init_storage_for_testing(storage_configuration, the_location_id=location_id)
+# test_ingest_item()
 
 asyncio.run(main(), debug=None)
---
+++ src/ska_dlm_client/directory_watcher/testing.py
@@ -0,0 +1,86 @@
+"""Code related to testing of watcher."""
+
+import logging
+import sys
+
+from ska_dlm_client.directory_watcher.configuration_details import WatchConfiguration
+from ska_dlm_client.openapi import api_client, configuration
+from ska_dlm_client.openapi.dlm_api import ingest_api, storage_api
+
+logger = logging.getLogger(__name__)
+
+
+def init_location_for_testing(storage_configuration: configuration) -> str:
+    """Perform location initialisation to be used when testing."""
+    with api_client.ApiClient(storage_configuration) as the_api_client:
+        api_storage = storage_api.StorageApi(the_api_client)
+
+        # get the location_id
+        response = api_storage.query_location_storage_query_location_get(
+            location_name=WatchConfiguration.LOCATION_NAME
+        )
+        logger.info("query_location_response: %s", response)
+        if not isinstance(response, list):
+            logger.error("Unexpected response from query_location_storage")
+            sys.exit(1)
+        if len(response) == 1:
+            the_location_id = response[0]["location_id"]
+        else:
+            response = api_storage.init_location_storage_init_location_post(
+                location_name=WatchConfiguration.LOCATION_NAME,
+                location_type=WatchConfiguration.LOCATION_TYPE,
+                location_country=WatchConfiguration.LOCATION_COUNTRY,
+                location_city=WatchConfiguration.LOCATION_CITY,
+                location_facility=WatchConfiguration.LOCATION_FACILITY,
+            )
+            the_location_id = response
+        logger.info("location_id: %s", the_location_id)
+    return the_location_id
+
+
+def init_storage_for_testing(storage_configuration: configuration, the_location_id: str) -> str:
+    """Perform storge initialisation to be used when testing."""
+    assert the_location_id is not None
+    with api_client.ApiClient(storage_configuration) as the_api_client:
+        api_storage = storage_api.StorageApi(the_api_client)
+        # Get the storage_id
+        response = api_storage.query_storage_storage_query_storage_get(
+            storage_name=WatchConfiguration.STORAGE_NAME
+        )
+        if not isinstance(response, list):
+            logger.error("Unexpected response from query_storage_storage")
+            sys.exit(1)
+        if len(response) == 1:
+            the_storage_id = response[0]["storage_id"]
+        else:
+            response = api_storage.init_storage_storage_init_storage_post(
+                storage_name=WatchConfiguration.STORAGE_NAME,
+                location_name=WatchConfiguration.LOCATION_NAME,
+                location_id=the_location_id,
+                storage_type=WatchConfiguration.STORAGE_TYPE,
+            )
+            the_storage_id = response
+        logger.info("storage_id: %s", the_storage_id)
+
+        # Setup the storage config
+        response = api_storage.create_storage_config_storage_create_storage_config_post(
+            storage_id=the_storage_id, config=WatchConfiguration.STORAGE_CONFIG
+        )
+        storage_config_id = response
+        logger.info("storage_config_id: %s", storage_config_id)
+    return the_storage_id
+
+
+def test_ingest_item(ingest_configuration: configuration, storage_id: str):
+    """Test ingesting a single item."""
+    with api_client.ApiClient(ingest_configuration) as the_api_client:
+        api_ingest = ingest_api.IngestApi(the_api_client)
+        the_path = "k"
+        response = api_ingest.register_data_item_ingest_register_data_item_post(
+            item_name=the_path,
+            uri=the_path,
+            storage_name=WatchConfiguration.STORAGE_NAME,
+            storage_id=storage_id,
+            eb_id=WatchConfiguration.EB_ID,
+        )
+        logger.info("register_data_item_response: %s", response)
