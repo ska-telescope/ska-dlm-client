ca5a9ed1f831167e60af61aac70578023ca932c8
Mark Boulton <mark.boulton@uwa.edu.au>
2024-11-22 11:57:35 +0800

    YAN-1890: Read metadata for a data product or generate if not found.

--- tests/directory_watcher/test_data_product_metadata.py
+++ tests/directory_watcher/test_data_product_metadata.py
@@ -1,12 +1,19 @@
 """Metadata related tests."""
 
 import os
+import shutil
 from shutil import copytree
 
 import pytest
 
 from ska_dlm_client.directory_watcher import config
 from ska_dlm_client.directory_watcher.data_product_metadata import DataProductMetadata
+
+METADATA_FILE_EB_ID = "eb-m001-20191031-12345"
+TEST_DATA_PRODUCT_FILE = "a_data_product.ext"
+TEST_DATA_PRODUCT_DIRECTORY = "some_path"
+TEST_EB_ID = "eb-m001-20191031-12345"
+UNKNOWN_EB_ID = "unknown-metadata-"
 
 
 @pytest.fixture(autouse=True)
@@ -22,11 +29,29 @@ def datadir(tmp_path, request):
 
 def test_data_product_metadata(tmp_path):
     """Test the DataProductMetadata class."""
+    # Raises exception when file not found
     with pytest.raises(FileNotFoundError):
         DataProductMetadata(f"{tmp_path}/a-dummy-file")
 
-    dpm = DataProductMetadata(f"{tmp_path}/{config.METADATA_FILENAME}")
-    assert dpm.get_execution_block_value() == "eb-m001-20191031-12345"
+    full_dp_file = f"{tmp_path}/{TEST_DATA_PRODUCT_FILE}"
+    full_metadata_file = f"{tmp_path}/{config.METADATA_FILENAME}"
 
-    dpm = DataProductMetadata(f"{tmp_path}/invalid-{config.METADATA_FILENAME}")
-    assert dpm.get_execution_block_value() is None
+    # Given a data product load the metadata file and check the eb_id
+    dpm = DataProductMetadata(full_dp_file)
+    assert dpm.get_execution_block_value() == TEST_EB_ID
+
+    # If a directory does not contain metadata check that some is created
+    full_dp_dir = f"{tmp_path}/{TEST_DATA_PRODUCT_DIRECTORY}"
+    dpm = DataProductMetadata(full_dp_dir)
+    assert dpm.get_execution_block_value().startswith(UNKNOWN_EB_ID)
+
+    # If a directory does contain metadata check it is found and loaded
+    dst_file = os.path.join(full_dp_dir, config.METADATA_FILENAME)
+    shutil.copyfile(src=full_metadata_file, dst=dst_file)
+    dpm = DataProductMetadata(full_dp_dir)
+    assert dpm.get_execution_block_value() == TEST_EB_ID
+
+    # Given a data product load the metadata file and check the eb_id
+    os.remove(full_metadata_file)
+    dpm = DataProductMetadata(full_dp_file)
+    assert dpm.get_execution_block_value().startswith(UNKNOWN_EB_ID)
