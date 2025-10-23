8fbc71ef3ed49570ed7b0ad0cb56f37cd4ed73ac
Andreas Wicenec <awicenec@gmail.com>
2025-10-23 11:33:50 +0800

    Added option to startup and remote config

--- src/ska_dlm_client/register_storage_location/main.py
+++ src/ska_dlm_client/register_storage_location/main.py
@@ -17,7 +17,66 @@ LOCATION_TYPE = "local-dev"
 LOCATION_COUNTRY = "AU"
 LOCATION_CITY = "Kensington"
 LOCATION_FACILITY = "local"
-STORAGE_CONFIG = {"name": "data", "type": "alias", "parameters": {"remote": "/"}}
+RCLONE_CONFIG = {"name": "data", "type": "alias", "parameters": {"remote": "/tmp"}}
+RCLONE_CONFIG_REMOTE = {"name": "dlm-watcher", "type": "sftp", "parameters":
+    {
+        "host": "dlm_directory_watcher",
+        "key_file": "/root/.ssh/id_rsa.pem",
+        "shell_type": "unix",
+        "type": "sftp",
+        "user": "ska-dlm"
+    }}
+STORAGE_INTERFACE = "posix"
+STORAGE_TYPE = "filesystem"
+
+
+def init_location_for_testing(storage_configuration: Configuration) -> str:
+    """Perform location initialisation to be used when testing."""
+    with api_client.ApiClient(storage_configuration) as the_api_client:
+        api_storage = storage_api.StorageApi(the_api_client)
+
+        # get the location_id
+        response = api_storage.query_location(location_name=LOCATION_NAME)
+        logger.info("query_location response: %s", response)
+        if not isinstance(response, list):
+            logger.error("Unexpected response from query_location_storage")
+            sys.exit(1)
+        if len(response) == 1:
+            the_location_id = response[0]["location_id"]
+            logger.info("location already exists in DLM")
+        else:
+            response = api_storage.init_location(
+                location_name=LOCATION_NAME,
+                location_type=LOCATION_TYPE,
+...back 1 page
+"""Initialize a location and a storage."""
+
+import argparse
+import logging
+import sys
+
+from ska_dlm_client.openapi import api_client
+from ska_dlm_client.openapi.configuration import Configuration
+from ska_dlm_client.openapi.dlm_api import storage_api
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+# Constants that can be used for testing.
+LOCATION_NAME = "local-dev"
+LOCATION_TYPE = "local-dev"
+LOCATION_COUNTRY = "AU"
+LOCATION_CITY = "Kensington"
+LOCATION_FACILITY = "local"
+RCLONE_CONFIG = {"name": "data", "type": "alias", "parameters": {"remote": "/tmp"}}
+RCLONE_CONFIG_REMOTE = {"name": "dlm-watcher", "type": "sftp", "parameters":
+    {
+        "host": "dlm_directory_watcher",
+        "key_file": "/root/.ssh/id_rsa.pem",
+        "shell_type": "unix",
+        "type": "sftp",
+        "user": "ska-dlm"
+    }}
 STORAGE_INTERFACE = "posix"
 STORAGE_TYPE = "filesystem"
 
@@ -55,6 +114,7 @@ def init_storage_for_testing(
     storage_configuration: Configuration,
     storage_root_directory: str,
     the_location_id: str,
+    rclone_config:str = RCLONE_CONFIG
 ) -> str:
     """Perform storage initialisation to be used when testing."""
     assert the_location_id is not None
@@ -84,7 +144,108 @@ def init_storage_for_testing(
 
         # Setup the storage config. Doesn't matter if it has been set before.
         response = api_storage.create_storage_config(
-            body=STORAGE_CONFIG,
+            body=rclone_config,
+            storage_id=the_storage_id,
+            storage_name=storage_name,
+            config_type="rclone",
+        )
+root@27623ffd3563:/app/.venv/lib/python3.12/site-packages/ska_dlm_client/register_storage_location# cat main.py 
+"""Initialize a location and a storage."""
+
+import argparse
+import logging
+import sys
+
+from ska_dlm_client.openapi import api_client
+from ska_dlm_client.openapi.configuration import Configuration
+from ska_dlm_client.openapi.dlm_api import storage_api
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+# Constants that can be used for testing.
+LOCATION_NAME = "local-dev"
+LOCATION_TYPE = "local-dev"
+LOCATION_COUNTRY = "AU"
+LOCATION_CITY = "Kensington"
+LOCATION_FACILITY = "local"
+RCLONE_CONFIG = {"name": "data", "type": "alias", "parameters": {"remote": "/tmp"}}
+RCLONE_CONFIG_REMOTE = {"name": "dlm-watcher", "type": "sftp", "parameters":
+    {
+        "host": "dlm_directory_watcher",
+        "key_file": "/root/.ssh/id_rsa.pem",
+        "shell_type": "unix",
+        "type": "sftp",
+        "user": "ska-dlm"
+    }}
+STORAGE_INTERFACE = "posix"
+STORAGE_TYPE = "filesystem"
+
+
+def init_location_for_testing(storage_configuration: Configuration) -> str:
+    """Perform location initialisation to be used when testing."""
+    with api_client.ApiClient(storage_configuration) as the_api_client:
+        api_storage = storage_api.StorageApi(the_api_client)
+
+        # get the location_id
+        response = api_storage.query_location(location_name=LOCATION_NAME)
+        logger.info("query_location response: %s", response)
+        if not isinstance(response, list):
+            logger.error("Unexpected response from query_location_storage")
+            sys.exit(1)
+        if len(response) == 1:
+            the_location_id = response[0]["location_id"]
+            logger.info("location already exists in DLM")
+        else:
+            response = api_storage.init_location(
+                location_name=LOCATION_NAME,
+                location_type=LOCATION_TYPE,
+                location_country=LOCATION_COUNTRY,
+                location_city=LOCATION_CITY,
+                location_facility=LOCATION_FACILITY,
+            )
+            the_location_id = response
+            logger.info("location created in DLM")
+        logger.info("location_id: %s", the_location_id)
+    return the_location_id
+
+
+def init_storage_for_testing(
+    storage_name: str,
+    storage_configuration: Configuration,
+    storage_root_directory: str,
+    the_location_id: str,
+    rclone_config:str = RCLONE_CONFIG
+) -> str:
+    """Perform storage initialisation to be used when testing."""
+    assert the_location_id is not None
+    with api_client.ApiClient(storage_configuration) as the_api_client:
+        api_storage = storage_api.StorageApi(the_api_client)
+        # Get the storage_id
+        response = api_storage.query_storage(storage_name=storage_name)
+        logger.info("query_storage response: %s", response)
+        if not isinstance(response, list):
+            logger.error("Unexpected response from query_storage_storage")
+            sys.exit(1)
+        if len(response) == 1:
+            the_storage_id = response[0]["storage_id"]
+            logger.info("storage_id already exists in DLM")
+        else:
+            response = api_storage.init_storage(
+                storage_name=storage_name,
+                storage_type=STORAGE_TYPE,
+                storage_interface=STORAGE_INTERFACE,
+                root_directory=storage_root_directory,
+                location_id=the_location_id,
+                location_name=LOCATION_NAME,
+            )
+            the_storage_id = response
+            logger.info("Storage created in DLM")
+        logger.info("storage_id: %s", the_storage_id)
+
+        # Setup the storage config. Doesn't matter if it has been set before.
+        response = api_storage.create_storage_config(
+            body=rclone_config,
             storage_id=the_storage_id,
             storage_name=storage_name,
             config_type="rclone",
@@ -103,10 +264,19 @@ def setup_testing(
     logger.info("Testing setup.")
     location_id = init_location_for_testing(storage_configuration)
     storage_id = init_storage_for_testing(
-        storage_name=storage_name,
+        storage_name=RCLONE_CONFIG["name"],
         storage_configuration=storage_configuration,
         storage_root_directory=storage_root_directory,
         the_location_id=location_id,
+        rclone_config=RCLONE_CONFIG
+    )
+
+    storage_id = init_storage_for_testing(
+        storage_name=RCLONE_CONFIG_REMOTE["name"],
+        storage_configuration=storage_configuration,
+        storage_root_directory=storage_root_directory,
+        the_location_id=location_id,
+        rclone_config=RCLONE_CONFIG_REMOTE
     )
     logger.info("location id %s and storage id %s", location_id, storage_id)
 
--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 cat /data/.ssh/authorized_keys > /home/ska-dlm/.ssh/authorized_keys
 chown -R ska-dlm:ska-dlm /home/ska-dlm/.ssh
+chown -R ska-dlm:ska-dlm /data/watch_dir
+chmod g+w /data/watch_dir
 /etc/init.d/ssh start
-su ska-dlm -c 'python3 -m ska_dlm_client.directory_watcher.main --directory-to-watch /data/watch_dir --storage-name data --storage-root-directory /data --use-polling-watcher --ingest-server-url "http://dlm_ingest:8001"  --readiness-probe-file "/tmp/dlm-client-ready" --skip-rclone-access-check-on-register'
+source /data/.venv/bin/activate
+python3 -m ska_dlm_client.directory_watcher.main --directory-to-watch /data/watch_dir --storage-name dlm-watcher --storage-root-directory /data --use-polling-watcher --ingest-server-url "http://dlm_ingest:8001" --migration-server-url http://dlm_migration:8004 --migration-destination-storage-name data --readiness-probe-file "/tmp/dlm-client-ready" --skip-rclone-access-check-on-register
