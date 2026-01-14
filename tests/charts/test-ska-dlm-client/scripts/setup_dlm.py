80b5e166a456e3bee102ea296c4d619b882a7408
Andreas Wicenec <awicenec@gmail.com>
2026-01-14 23:01:56 +0800

    added root_path to storage config

--- tests/charts/test-ska-dlm-client/scripts/setup_dlm.py
+++ tests/charts/test-ska-dlm-client/scripts/setup_dlm.py
@@ -35,7 +35,7 @@ LOCATION_TYPE = LocationType.LOW_INTEGRATION
 LOCATION_COUNTRY = LocationCountry.AU
 LOCATION_CITY = "Marksville"
 LOCATION_FACILITY = "local"  # TODO: query location_facility lookup table
-STORAGE_CONFIG = {"name": "dir-watcher", "type": "local", "parameters": {}}
+STORAGE_CONFIG = {"name": "dir-watcher", "type": "local", "root_path": "", "parameters": {}}
 STORAGE_INTERFACE = StorageInterface.POSIX
 STORAGE_TYPE = StorageType.FILESYSTEM
 
