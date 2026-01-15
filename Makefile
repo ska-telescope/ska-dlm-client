eddf1acf3e1e0321bf635b150f16fa13b3ed9f8c
Nadia Steyn <nadia.steyn.bsc@gmail.com>
2026-01-15 15:43:02 +0800

    DMAN-178: Correct server OCI image tag. Remove debugging logs. Siwtch watchers on by default on values.yaml.

--- Makefile
+++ Makefile
@@ -26,8 +26,8 @@ python-do-test:
 
 python-post-test: docker-compose-down
 
-# integration-test: docker-compose-up run-integration-test docker-compose-down
-integration-test: docker-compose-up run-integration-test
+integration-test: docker-compose-up run-integration-test docker-compose-down
+# integration-test: docker-compose-up run-integration-test
 
 run-integration-test:
 	export SERVER_IMAGE=$(DLM_SERVER_IMAGE) && $(DOCKER_COMPOSE) --file tests/integration/dlm_servers.docker-compose.yaml ps -a
--- src/ska_dlm_client/register_storage_location/main.py
+++ src/ska_dlm_client/register_storage_location/main.py
@@ -16,12 +16,6 @@ from ska_dlm_client.openapi.dlm_api import storage_api
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
-logger.info(
-    "[env] STORAGE_URL=%r INGEST_URL=%r SOURCE_NAME=%r",
-    os.getenv("STORAGE_URL"),
-    os.getenv("INGEST_URL"),
-    os.getenv("SOURCE_NAME"),
-)
 
 # Constants that can be used for testing.
 LOCATION_NAME = "ThisDLMClientLocationName"
@@ -62,10 +56,6 @@ def get_or_init_location(
         # get the location_id
         logger.info("Checking location: %s", location)
         api_storage.api_client.configuration.host = storage_url
-        logger.info(
-            "[get_or_init_location] calling query_location with host=%s",
-            api_storage.api_client.configuration.host,
-        )
         response = api_storage.query_location(location_name=location)
         logger.info("query_location response: %s", response)
         if not isinstance(response, list):
@@ -110,10 +100,6 @@ def get_or_init_storage(
         api_storage = storage_api.StorageApi(the_api_client)
         # Ensure storage API calls go to the storage service, not ingest
         api_storage.api_client.configuration.host = api_configuration.host
-        logger.info(
-            "[get_or_init_storage] calling query_storage with host=%s",
-            api_storage.api_client.configuration.host,
-        )
         # Get the storage_id
         response = api_storage.query_storage(storage_name=storage_name)
         logger.info("query_storage response: %s", response)
@@ -185,12 +171,6 @@ def setup_volume(  # pylint: disable=too-many-arguments, too-many-positional-arg
 ):
     """Register and configure a storage volume. This takes care of already existing volumes."""
     storage_url = storage_url or os.getenv("STORAGE_URL") or "http://dlm_storage:8003"
-    logger.info(
-        "[setup_volume] storage_url resolved to: %s", storage_url
-    )
-    logger.info(
-        "[setup_volume] api_configuration.host = %s", api_configuration.host
-    )
     if location_id is None:
         location_id = get_or_init_location(
             api_configuration, location=LOCATION_NAME, storage_url=storage_url
