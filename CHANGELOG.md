1aa4d4a4a3be31275a60cf778631e1aa22737e80
Nadia Steyn <nadia.steyn.bsc@gmail.com>
2026-02-06 16:01:16 +0800

    REL-2693: 1.2.2 patch release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Version History
 
-## Development
+## 1.2.2
 
 ### Updated
 
--- tests/dlm_client/test_ska_dlm_client.py
+++ tests/dlm_client/test_ska_dlm_client.py
@@ -25,4 +25,4 @@ def test_content(version: str) -> None:
 
     :param version: the version fixture
     """
-    assert version == "1.2.1"
+    assert version == "1.2.2"
