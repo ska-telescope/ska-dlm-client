f2de5f60f7925e7f3709081726180d2a403f0b49
Andreas Wicenec <awicenec@gmail.com>
2026-07-03 15:38:39 +0800

    DMAN-200: Add even more wait for the configdb_watcher

--- tests/integration/test_configdb_watcher.py
+++ tests/integration/test_configdb_watcher.py
@@ -455,6 +455,9 @@ def test_automatic_deletion(dlm_request_api, storage_configuration):
     test_dir = f"{WATCHER_SOURCE_DIR_ROOT}/product/{EB_ID}/ska-sdp/{PB_ID}/{ARB_MS}"
     # test -d <path> returns 0 if directory exists:
     result = subprocess.run(["docker", "exec", SRC_HOST, "test", "-d", test_dir])
-    log.info("STDOUT/STDERR of test for directory: %s / %s", result.stdout, result.stderr)
+    if result.returncode == 0:
+        log.info("Waiting even longer...")
+        sleep(40)
+        result = subprocess.run(["docker", "exec", SRC_HOST, "test", "-d", test_dir])
 
     assert result.returncode != 0, f"Directory {test_dir} still exists: {result.stdout}"
