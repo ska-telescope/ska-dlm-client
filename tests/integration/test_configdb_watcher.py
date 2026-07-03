d112a64b687b638faf5ba40af9b611f56c7ef4d8
Andreas Wicenec <awicenec@gmail.com>
2026-07-03 12:46:50 +0800

    DMAN-200: Extend sleep time to avoid intermittent test failures

--- tests/integration/test_configdb_watcher.py
+++ tests/integration/test_configdb_watcher.py
@@ -450,7 +450,7 @@ def test_automatic_deletion(dlm_request_api, storage_configuration):
     # avoid iterative HTTP round-trips to a single DB update, from the client-side.
 
     log.info("Sleep to give heuristics some time to do its thing.")
-    sleep(12)  # default poll interval of the heuristics is 10 seconds
+    sleep(20)  # default poll interval of the heuristics is 10 seconds
 
     test_dir = f"{WATCHER_SOURCE_DIR_ROOT}/product/{EB_ID}/ska-sdp/{PB_ID}"
     # test -d <path> returns 0 if directory exists:
