d85bd6d79c7bf57d98852632419c58ffea7722df
Andreas Wicenec <awicenec@gmail.com>
2026-07-03 16:15:54 +0800

    DMAN-200: We need to see what the heuristics engine is doing.

--- tests/integration/test_configdb_watcher.py
+++ tests/integration/test_configdb_watcher.py
@@ -455,11 +455,17 @@ def test_automatic_deletion(dlm_request_api, storage_configuration):
     test_dir = f"{WATCHER_SOURCE_DIR_ROOT}/product/{EB_ID}/ska-sdp/{PB_ID}"
     # test -d <path> returns 0 if directory exists:
     result = subprocess.run(["docker", "exec", SRC_HOST, "test", "-d", test_dir])
-    if result.returncode == 0:
+    counter = 0
+    while counter < 3:
         logs = subprocess.run(["docker", "logs", "dlm_heuristics"], capture_output=True, text=True)
         log.info("Logs from heuristics container: %s", logs.stdout)
         log.info("Waiting even longer...")
         sleep(40)
         result = subprocess.run(["docker", "exec", SRC_HOST, "test", "-d", test_dir])
-
+        if result.returncode != 0:
+            break
+        counter += 1
+    if result.returncode == 0:
+        logs = subprocess.run(["docker", "--tail", "500", "logs", "dlm_heuristics"], capture_output=True, text=True)
+        log.info("Still failing: More logs from heuristics container: %s", logs.stdout)
     assert result.returncode != 0, f"Directory {test_dir} still exists: {result.stdout}"
