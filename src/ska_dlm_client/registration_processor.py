4452be31ecefb1284f95e32889872c1870074a60
Nadia Steyn <nadia.steyn.bsc@gmail.com>
2026-07-31 09:29:43 +0800

    DMAN-287: Fixing some type hints.
    With Dave & Callan's help.

--- src/ska_dlm_client/registration_processor.py
+++ src/ska_dlm_client/registration_processor.py
@@ -81,9 +81,10 @@ class RegistrationProcessor:
         """
         self._config = config
         self.last_migration_result: str | None = None
-        (self.target_storage_id, self.target_storage_phase) = self._get_storage_info_from_name(
-            getattr(self._config, "target_name", None),
+        self.target_storage_id, self.target_storage_phase = self._get_storage_info_from_name(
+            self._config.target_name,
         )
+
         request_configuration = getattr(self._config, "request_configuration", None)
         with api_client.ApiClient(request_configuration) as the_api_client:
             self.api_request = request_api.RequestApi(the_api_client)
@@ -264,7 +265,7 @@ class RegistrationProcessor:
 
         return result
 
-    def _get_storage_info_from_name(self, storage_name: str) -> tuple[str, str] | None:
+    def _get_storage_info_from_name(self, storage_name: str) -> tuple[str | None, str | None] :
         """Get the storage_id and phase for a given storage_name.
 
         Args:
